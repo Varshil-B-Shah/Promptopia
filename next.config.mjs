4d092c2019218eeb2aa2b58e0ba163ed5486014d
Varshil Shah <varshil4247shah@gmail.com>
2024-07-13 17:29:09 +0530

    fixed useSearchParams

--- next.config.mjs
+++ next.config.mjs
@@ -20,7 +20,10 @@ const nextConfig = {
     return config;
   },
 };
-
-// module.exports = nextConfig;
+module.exports = {
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+};
 
 export default nextConfig;
